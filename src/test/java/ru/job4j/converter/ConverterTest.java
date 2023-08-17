59379551fbdd62123a8750695cc3f82c99d15cc5
Timur <timk01@mail.ru>
2023-08-17 19:42:38 +0300

    1. Модульные тесты. [#159435]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.converter;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void convertRubleToEuroSuccess() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.000_0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void convertRubleToEuroFail() {
+        float in = 140;
+        float expected = 2.2F;
+        float out = Converter.rubleToEuro(in);
+        float offset = 0.000_0001f;
+        assertNotEquals(expected, out, offset);
+    }
+
+    @Test
+    void convertRubleToDollarSuccess() {
+        float in = 180;
+        float expected = 3;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.000_0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void convertRubleToDollarFail() {
+        float in = 180;
+        float expected = 3.5F;
+        float out = Converter.rubleToDollar(in);
+        float offset = 0.000_0001f;
+        assertNotEquals(expected, out, offset);
+    }
+
+    @Test
+    void convertRubleOfValue0ToDollarSuccess() {
+        float in = 0;
+        float expected = 0;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.000_0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void convertRubleOfValue0ToEuroSuccess() {
+        float in = 0;
+        float expected = 0;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.000_0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+}
