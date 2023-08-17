00d54fbb34bb25c762be0ce7de472e0de0607e3f
Timur <timk01@mail.ru>
2023-08-17 10:47:29 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/calculate/Out.java
@@ -0,0 +1,10 @@
+package ru.job4j.calculate;
+
+public class Out {
+    public static void main(String[] args) {
+        String label = "I ";
+        label = label + "like ";
+        label += "Java!";
+        System.out.println(label);
+    }
+}
