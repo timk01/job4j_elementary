4de28dad784dae7811e1d77cb0cd0dc0671df66f
Timur <timk01@mail.ru>
2023-08-18 11:37:19 +0300

    1. Модульные тесты. [#159435 #437659]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -1,64 +1,43 @@
 package ru.job4j.converter;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 class ConverterTest {
 
     @Test
-    void convertRubleToEuroSuccess() {
+    void whenConvert140RoublesThen2Euros() {
         float in = 140;
         float expected = 2;
         float out = Converter.rubleToEuro(in);
-        float eps = 0.000_0001f;
+        float eps = 0.0001f;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
 
     @Test
-    void convertRubleToEuroFail() {
-        float in = 140;
-        float expected = 2.2F;
-        float out = Converter.rubleToEuro(in);
-        float eps = 0.000_0001f;
-        assertThat(out).isNotCloseTo(expected, withPrecision(eps));
-    }
-
-    @Test
-    void convertRubleToDollarSuccess() {
+    void whenConvert180RoublesThen3Dollars() {
         float in = 180;
         float expected = 3;
         float out = Converter.rubleToDollar(in);
-        float eps = 0.000_0001f;
+        float eps = 0.0001f;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
 
     @Test
-    void convertRubleOfValue0ToDollarSuccess() {
+    void whenConvert0RoublesThen0Euros() {
         float in = 0;
         float expected = 0;
         float out = Converter.rubleToDollar(in);
-        float eps = 0.000_0001f;
+        float eps = 0.0001f;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
 
     @Test
-    void convertRubleToDollarFail() {
+    void whenConvert0RoublesThen0Dollars() {
         float in = 180;
         float expected = 3.5F;
         float out = Converter.rubleToDollar(in);
-        float eps = 0.000_0001f;
+        float eps = 0.0001f;
         assertThat(out).isNotCloseTo(expected, withPrecision(eps));
-    }
-
-    @Test
-    void convertRubleOfValue0ToEuroSuccess() {
-        float in = 0;
-        float expected = 0;
-        float out = Converter.rubleToDollar(in);
-        float eps = 0.000_0001f;
-        assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
 }
