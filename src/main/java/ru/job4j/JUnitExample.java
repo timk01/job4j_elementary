bb1b9447775538c0b5f3fe0a61f4fe8c0bc626b0
Timur <timk01@mail.ru>
2023-08-20 09:40:38 +0300

    Removed junit 4 and hamcrest Added mock tests wih annotations

---
+++ src/main/java/ru/job4j/JUnitExample.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class JUnitExample {
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/ru/job4j/JUnitExampleTest.java
@@ -0,0 +1,51 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.*;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.*;
+
+class JUnitExampleTest {
+
+    @Test
+    void when5And5Then25() {
+        JUnitExample jUnitExample = new JUnitExample();
+        int result = jUnitExample.multiply(5, 5);
+        int expected = 25;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @RepeatedTest(2)
+    void when6And6Then36() {
+        JUnitExample jUnitExample = new JUnitExample();
+        int result = jUnitExample.multiply(6, 6);
+        int expected = 36;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("When 7 multi 7 should be 49")
+    void when7And7Then49() {
+        JUnitExample example = new JUnitExample();
+        int result = example.multiply(7, 7);
+        assertThat(result).isEqualTo(49);
+    }
+
+    @Disabled("smh is wrong with method")
+    @Test
+    void when10And10Then25() {
+        JUnitExample example = new JUnitExample();
+        int result = example.multiply(10, 10);
+        assertThat(result).isEqualTo(100);
+    }
+
+    @Timeout(value = 6000, unit = TimeUnit.MILLISECONDS)
+    @Test
+    void when15And15hen225() throws InterruptedException {
+        Thread.sleep(5000);
+        JUnitExample example = new JUnitExample();
+        int result = example.multiply(15, 15);
+        assertThat(result).isEqualTo(225);
+    }
+}
