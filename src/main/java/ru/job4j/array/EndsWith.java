f33718003d55882e568150389fab51d6a0bd2f86
Timur <timk01@mail.ru>
2023-08-23 23:04:14 +0300

    6.4.3. Слово заканчивается на ... [#156317]

---
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        if (word.length < post.length) {
+            return false;
+        }
+        boolean result = true;
+        for (int i = 0; i < post.length; i++) {
+            if (word[word.length - 1 - i] != post[post.length - 1 - i]) {
+                return false;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/array/EndsWithTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EndsWithTest {
+    @Test
+    public void whenEndWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenNotEndWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'a'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void whenEndWithPrefixAndPrefixIsBiggerThanWordThenFalse() {
+        char[] word = {'H', 'e', 'l'};
+        char[] pref = {'H', 'e', 'H', 'e', 'l'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void whenNotEndWithPrefixAndPrefixIsBiggerThanWordThenFalse() {
+        char[] word = {'L', 'o'};
+        char[] pref = {'H', 'e', 'l', 'l', 'o'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result).isFalse();
+    }
+}
