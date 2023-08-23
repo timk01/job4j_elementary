17d01bec84a9db2bf7ac7c04a193dd54fce9b9d4
Timur <timk01@mail.ru>
2023-08-23 16:20:48 +0300

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,12 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("length of ages is: " + ages.length);
+
         String[] surnames = new String[100500];
+        System.out.println("length of surnames is: " + surnames.length);
+
         float[] prices = new float[40];
+        System.out.println("length of prices is: " + prices.length);
     }
 }
