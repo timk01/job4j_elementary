96097a7859c9b80d31869c1b81858555cc6103e8
Timur <timk01@mail.ru>
2023-08-24 20:58:07 +0300

    6.6.0. Двумерный массив [#226796]

---
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class Sequence {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        array[0][0] = 1;
+        array[0][1] = 2;
+        array[0][2] = 3;
+        array[1][0] = 4;
+        array[1][1] = 10;
+        array[1][2] = 11;
+        array[2][0] = 12;
+        array[2][1] = 13;
+        array[2][2] = 14;
+    }
+}
