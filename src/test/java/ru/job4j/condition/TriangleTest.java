71fe5afb2ef852076fec99268e2143ef6d3a32b4
Timur <timk01@mail.ru>
2025-06-27 20:40:22 +0300

    4.3.5. Множественное логическое выражение И [#9461 #438605] - дополнил праметризированными тестами

--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,6 +1,12 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TriangleTest {
@@ -23,4 +29,29 @@ class TriangleTest {
         assertThat(result).isFalse();
     }
 
+    static Stream<Arguments> triangleExistsData() {
+        return Stream.of(
+                Arguments.of(3, 4, 5),
+                Arguments.of(7, 10, 5)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("triangleExistsData")
+    void whenTriangleExistsThenTrue(double ab, double ac, double bc) {
+        assertThat(Triangle.exist(ab, ac, bc)).isTrue();
+    }
+
+    static Stream<Arguments> triangleDoesNotExistsData() {
+        return Stream.of(
+                Arguments.of(10, 2, 7),
+                Arguments.of(1, 2, 3)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("triangleDoesNotExistsData")
+    void whenTriangleDoesNotExistThenFalse(double ab, double ac, double bc) {
+        assertThat(Triangle.exist(ab, ac, bc)).isFalse();
+    }
 }
