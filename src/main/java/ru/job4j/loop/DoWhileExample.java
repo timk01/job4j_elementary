1bbab0eeb853d3fb827d1b153642994dd98fb47c
Timur <timk01@mail.ru>
2025-08-13 21:01:54 +0300

    6.0. Цикл do-while [#504919 #439052] - вставил нормальную обработку сканера;

--- src/main/java/ru/job4j/loop/DoWhileExample.java
+++ src/main/java/ru/job4j/loop/DoWhileExample.java
@@ -6,9 +6,14 @@ public class DoWhileExample {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int password;
+        int password = 0;
         do {
             System.out.println("Введите пароль: ");
+            if (!scanner.hasNextInt()) {
+                System.out.println("Ошибка: введите число!");
+                scanner.next();
+                continue;
+            }
             password = scanner.nextInt();
         } while (password != 555);
         System.out.println("Пароль введён успешно!");
