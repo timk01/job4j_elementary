4f68b31f5f0bc416de49fd0b9fc4c5515d3595f2
Timur <timk01@mail.ru>
2025-08-14 13:28:48 +0300

    6.0.1. Объявление массива. [#3613 #439147] - добавил оригинальное объявление разных массивов по заданию

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,6 +2,10 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+
         String[] names = new String[4];
         names[0] = "Boris Borisovich";
         names[1] = "Petr Petrovich}";
