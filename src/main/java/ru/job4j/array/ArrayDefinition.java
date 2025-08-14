58aa9a412c21524117990d0b2ae3dce68e28643d
Timur <timk01@mail.ru>
2025-08-14 14:26:47 +0300

    6.0.2. Размер массива [#333582 #439152] - вернул оригинальный вызов длины массива по заданию

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,8 +3,11 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("length of ages is: " + ages.length);
         String[] surnames = new String[100500];
+        System.out.println("length of surnames is: " + surnames.length);
         float[] prices = new float[40];
+        System.out.println("length of prices is: " + prices.length);
 
         String[] names = new String[4];
         names[0] = "Boris Borisovich";
