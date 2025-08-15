9d6cec862eb85d04eac4d1ceb9001ad36763f0a9
Timur <timk01@mail.ru>
2025-08-15 20:27:46 +0300

    6.5.3. Сортировка выборкой. [#156320 #439493] - добавил оригинальный сорт-метод на сортировк выборкой; - добавил клаччическую сортировку пузырьком

---
+++ src/main/java/ru/job4j/array/SortBubbleOriginalAlgorithm.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class SortBubbleOriginalAlgorithm {
+
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = 0; j < data.length - 1 - i; j++) {
+                if (data[j] > data[j + 1]) {
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+        return data;
+    }
+}
---
+++ src/main/java/ru/job4j/array/SortSelectedOriginalAlgorithm.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+public class SortSelectedOriginalAlgorithm {
+
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = data[i];
+            int minIndex = i;
+            for (int j = 1 + i; j < data.length; j++) {
+                if (min > data[j]) {
+                    min = data[j];
+                    minIndex = j;
+                }
+            }
+            if (data[i] != min) {
+                int temp = data[i];
+                data[i] = min;
+                data[minIndex] = temp;
+            }
+        }
+        return data;
+    }
+}
