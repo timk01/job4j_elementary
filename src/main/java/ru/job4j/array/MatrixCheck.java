36ec6dce8ba6f1323d2661980cbdb20f9bebbaa0
Timur <timk01@mail.ru>
2025-08-18 20:14:32 +0300

    6.6.4. Двумерный массив. Таблица умножения. [#33491 #439541] - причесал код

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,11 +3,11 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-            for (int j = 0; j < board.length; j++) {
-                if (board[row][j] == ' ') {
-                    result = false;
-                    break;
-                }
+        for (int j = 0; j < board.length; j++) {
+            if (board[row][j] == ' ') {
+                result = false;
+                break;
+            }
         }
         return result;
     }
@@ -15,9 +15,9 @@ public class MatrixCheck {
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
-                if (board[i][column] == ' ') {
-                    result = false;
-                    break;
+            if (board[i][column] == ' ') {
+                result = false;
+                break;
             }
         }
         return result;
