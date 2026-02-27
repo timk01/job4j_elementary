34107e1b22d2a8f848512cedc0f8e21120c53e13
Timur <timk01@mail.ru>
2026-02-27 09:56:34 +0300

    1. Модульные тесты. [#159435 #437659] - добавил несколько тестов на новые методы, включая парам. тест на эквивалентность методов

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -1,6 +1,13 @@
 package ru.job4j.converter;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import ru.job4j.condition.Triangle;
+
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.*;
 
 class ConverterTest {
@@ -33,11 +40,45 @@ class ConverterTest {
     }
 
     @Test
-    void whenConvert0RoublesThen0Dollars() {
+    void whenConvert180RoublesThen3dot5Dollars() {
         float in = 180;
         float expected = 3.5F;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
         assertThat(out).isNotCloseTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert160RoublesThen2dot29Euros() {
+        double in = 160;
+        double expected = 2.29;
+        double out = Converter.roundedTill2DigitsRubleToEuro(in);
+        double eps = 0.01f;
+        assertThat(out).isCloseTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert160RoublesThen2dot29EurosUsingAlternativeImplementation() {
+        double in = 160;
+        double expected = 2.29;
+        double out = Converter.preciseRubleToEuro(in);
+        double eps = 0.01f;
+        assertThat(out).isCloseTo(expected, withPrecision(eps));
+    }
+
+    static Stream<Arguments> roubleData() {
+        return Stream.of(
+                Arguments.of(160),
+                Arguments.of(170)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("roubleData")
+    void whenMethodsAreEquivalent(double firstVal) {
+        double result1 = Converter.roundedTill2DigitsRubleToEuro(firstVal);
+        double result2 = Converter.preciseRubleToEuro(firstVal);
+        double eps = 0.0001;
+        assertThat(result1).isCloseTo(result2, withPrecision(eps));
     }
 }
